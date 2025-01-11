0fea08a501add34cc34e6faceaa44d177e156010
Mark-Lasfar <marklasfar@gmail.com>
2025-01-10 20:08:07 -0600

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export const SENDER_NAME = process.env.SENDER_NAME || 'support'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
+
+export const USER_ROLES = ['Admin', 'User']
+export const COLORS = ['Gold', 'Green', 'Red']
+export const THEMES = ['Light', 'Dark', 'System']
