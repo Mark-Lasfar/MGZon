d4a2c741b7707a76c4938441d562e8f1355345e0
Mark EL-asfar <140356054+Mark-Lasfar@users.noreply.github.com>
2025-01-19 16:41:09 -0600

    Update constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
-export const SENDER_NAME = process.env.SENDER_NAME || 'support'
+export const SENDER_NAME = process.env.SENDER_NAME || 'MGZon'
 export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
 export const SERVER_URL =
-  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+  process.env.NEXT_PUBLIC_SERVER_URL || 'https://mg-zon.vercel.app'
 
 
 export const USER_ROLES = ['Admin', 'User']
