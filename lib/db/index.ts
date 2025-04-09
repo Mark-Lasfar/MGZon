56997965d4541764f0195d7728c65e5f791bb05b
Mark EL-asfar <140356054+Mark-Lasfar@users.noreply.github.com>
2025-04-09 00:09:01 -0400

    Update index.ts

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,14 +3,22 @@ import mongoose from 'mongoose'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const cached = (global as any).mongoose || { conn: null, promise: null }
 
-export const connectToDatabase = async (MONGODB_URI = process.env.MONGODB_URI) => {
+export const connectToDatabase = async (
+  MONGODB_URI = process.env.MONGODB_URI
+) => {
   if (cached.conn) return cached.conn
 
   if (!MONGODB_URI) throw new Error('MONGODB_URI is missing')
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI)
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'mgzon',
+    bufferCommands: false,
+  })
 
   cached.conn = await cached.promise
+
+  require('@/lib/db/models/contact.model')
+  require('@/lib/db/models/rating.model')
 
   return cached.conn
 }
