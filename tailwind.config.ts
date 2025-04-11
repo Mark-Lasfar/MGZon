df9d011acbb3543be5664cce578ae116a9ceec74
Mark EL-asfar <140356054+Mark-Lasfar@users.noreply.github.com>
2025-04-11 17:00:36 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ import { withUt } from 'uploadthing/tw'
 const config: Config = withUt({
   darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/uploadthing/dist/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
@@ -56,6 +56,25 @@ const config: Config = withUt({
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+        'fadeIn': {
+          from: { opacity: '0', transform: 'translateY(10px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        'fadeIn': 'fadeIn 0.6s ease-out forwards',
       },
     },
   },
