ca6afbcefef58be07a47c95f019ef16dfd6d5997
Mark EL-asfar <140356054+Mark-Lasfar@users.noreply.github.com>
2025-04-12 20:22:03 -0400

    Update theme-provider.tsx

--- components/shared/theme-provider.tsx
+++ components/shared/theme-provider.tsx
@@ -3,10 +3,8 @@
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ColorProvider } from './color-provider'
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <NextThemesProvider {...props}>
       <ColorProvider>{children}</ColorProvider>
